10b98f17b0ff02072724e65609230adaccd4c11b
Petty <pettwil1131@gmail.com>
2021-06-15 17:07:45 -0500

    Adds user, listing, image seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# User.create!([
+#   {first_name: "William", last_name: "Petty", email: "pettwil1131@gmail.com", phone_number: "5555555555", address: "1131 Harbor River Cv", city: "Memphis", state: "TN", about_me: "yadayada yadayadayada yadayada yadayadayadayada yadayadayada yadayada", image_url: "https://avatars.githubusercontent.com/u/77342332?v=4", host: true, password_digest: "$2a$12$hPKcnTLyaFzCclDJQRruIOEKuuSEZzFylxpCBT13DuX4bfOccV0nW"}
+# ])
+
+# Listing.create!([
+#   {user_id: 1, title: "740 Acres in Parkin, AR", description: "There is a 4 day reservation during Christmas Week, December 20-23, 2021. This is primarily rice field hunting, with some fields in beans this year. There are 4 pit blinds to hunt that will all be available. There is an available guide service upon request, but only for the first 3 days (not available December 23). Unfortunately, we will not be able to split this into individual days, so it's 4 days or bust! Contact Will Petty below for more information!", address: "774 State HWY 306, Parkin, AR 72373", availability: "December 20-23, 2021", price: "1200"}
+# ])
+
+# Image.create!([
+#   {listing_id: 1, url: "https://www.louisianasportsman.com/wp-content/uploads/2014/01/p1391188684-1.jpg"}
+# ])
