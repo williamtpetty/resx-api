55fd4a0b478212d85a2b676f15a1d4bc95f595c1
Petty <pettwil1131@gmail.com>
2021-07-06 15:00:49 -0500

    Adds created_at to listing serializer

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,6 @@
 class Image < ApplicationRecord
 
   validates :listing_id, :url, presence: true
-  # validates :listing_id, numericality: true # not sure on this one
 
   belongs_to :listing
 end
--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -1,5 +1,5 @@
 class ListingSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :address, :latitude, :longitude, :availability, :price
+  attributes :id, :title, :description, :address, :latitude, :longitude, :availability, :price, :created_at
 
   has_many :images
   belongs_to :user
