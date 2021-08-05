b32cb028f6befe4f6828d5cdf1b23da85f803ecc
Petty <pettwil1131@gmail.com>
2021-08-05 11:53:47 -0500

    Cors.rb file adjustments to allow for heroku access

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'resxleasing.netlify.app'
+    origins 'localhost:8080', 'resxleasing.heroku.com', 'resxleasing.netlify.app'
 
     resource '*',
       headers: :any,
